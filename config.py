21f2e4a9845dbce14a6ccde48fe96f0298ca3bcc
Mody <143101587+Source-Ze@users.noreply.github.com>
2023-11-18 14:02:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from Plugin.api import *
 import os 
 
 SUDO = 6673736816 # admin or sudo id
-CHANNLS_BOT = ['UI_XB'] # bot channls 
+CHANNLS_BOT = ['UI_XB] # bot channls 
+API_KEY = '6365930995:AAHMGYrrkZxCbOTSDj9NWbHjdnEgWfg9dwQ'  # API_KEY OR BOT_TOKN 
 
 
 if not os.path.exists('./.session'):
@@ -15,9 +16,9 @@ if not os.path.exists('./.databesas'):
 
 app = Client(
     '.session/rad',
-    bot_token="6365930995:AAHMGYrrkZxCbOTSDj9NWbHjdnEgWfg9dwQ", # API_KEY 
-    api_hash="fe77fbf0cae9f7f5ece37659e2466cf1", # UserBot api_hahs
-    api_id="21627756" # UserBot api_id 
+    bot_token=API_KEY, # API_KEY 
+    api_hash='fe77fbf0cae9f7f5ece37659e2466cf1', # UserBot api_hahs
+    api_id='21627756' # UserBot api_id 
 )
 
 datas, apiV = databesas(), TempMailApi()
